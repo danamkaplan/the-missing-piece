a219b46138136312ba04a66dacb9d84481cdf15f
Dana Kaplan <danamkaplan@gmail.com>
2017-01-18 00:58:14 -0800

    first data run

--- gather_data.py
+++ gather_data.py
@@ -2,13 +2,16 @@ from boardgamegeek import BoardGameGeek
 import pickle
 import time
 from tqdm import tqdm
+import random
+
 
 # BGG object
 bgg = BoardGameGeek()
 
 games = {}
 missing_game_ids = []
-for id_ in tqdm(xrange(1,100)):
+
+for id_ in tqdm(random.sample(range(1, 88164), 5000)):
     try:
         game = bgg.game(game_id = id_)
         games[id_] = game.data()
