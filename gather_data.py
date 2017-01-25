dd5bc1b7c93c806763344d6efef4e9734f1ff24e
Dana Kaplan <danamkaplan@gmail.com>
2017-01-25 01:53:05 +0000

    testing memory leakage

--- gather_data.py
+++ gather_data.py
@@ -4,7 +4,7 @@ import json
 import sys
 
 # BGG object
-bgg = BoardGameGeek(requests_per_minute=60)
+bgg = BoardGameGeek(requests_per_minute=90)
 
 
 # latest game 218780
@@ -13,6 +13,11 @@ def write_json(first, last, game_dict):
     loc = './data/games_{}_{}.json'.format(first, last)
     f = open(loc, 'wb')
     f.write(j)
+    f.close()
+
+def write_missing(missing_game_ids, start, stop):    
+    f = open('./data/missing_games_{}_{}.txt'.format(start, stop), 'wb')
+    f.write(', '.join([str(i) for i in missing_game_ids]))
     f.close()
 
 def pull_game_data(start, stop):
@@ -25,22 +30,14 @@ def pull_game_data(start, stop):
         try:
             game = bgg.game(game_id = id_)
             games[id_] = game.data()
-            found_game_ids.append(id_)
+            game = None
         except:
             missing_game_ids.append(id_)
         if loops == 10000:
             loops = 0
             write_json(id_, id_, games)
             games = {}
-
-
-    write_json(start, 'end', games)
-    f = open('./data/found_games_{}_{}.txt'.format(start, stop), 'wb')
-    f.write(', '.join([str(i) for i in found_game_ids]))
-    f.close()
-    f = open('./data/missing_games_{}_{}.txt'.format(start, stop), 'wb')
-    f.write(', '.join([str(i) for i in missing_game_ids]))
-    f.close()
+            write_missing(missing_game_ids, id_, id_)
 
 if __name__=='__main__':
     start = int(sys.argv[1])
