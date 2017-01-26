9b20d22a7e4e82dfa408809aee117af70f3619a0
Dana Kaplan <danamkaplan@gmail.com>
2017-01-26 08:33:13 -0800

    will parse the xml later

--- gather_data.py
+++ gather_data.py
@@ -3,6 +3,9 @@ from collections import defaultdict
 from tqdm import tqdm
 import json
 import sys
+import requests
+from time import sleep
+from xml.etree import ElementTree
 
 # BGG object
 bgg = BoardGameGeek(requests_per_minute=90)
@@ -40,6 +43,116 @@ def pull_game_data(start, stop):
             games = {}
             write_missing(missing_game_ids, id_, id_)
 
+
+def evaluate_xml(g):
+    #turn xml pull into the same format as json
+    game_dict = {}
+    game_dict['mechanics'] =unpack_list(g['boardgamemechanic'])
+    game_dict['playingtime'] = int(g['playingtime'])
+    game_dict['id'] = int(g['@objectid'])
+    game_dict['image'] = g['image']
+    #game_dict['expansions'] 
+    #game_dict['artists']
+    game_dict['yearpublished'] = g['yearpublished']
+    game_dict['maxplayers'] = int(g['maxplayers'])
+    game_dict['thumbnail'] = g['thumbnail']
+    game_dict['publishers'] = unpack_list(g['boardgamepublisher'])
+    game_dict['families'] = unpack_list(g['boardgamefamily'])
+    game_dict['description'] = g['description']
+    game_dict['minplayers'] = int(g['minplayers'])
+    #game_dict['expansion']
+    #game_dict['implementations'] = 
+    game_dict['designers'] = unpack_list(g['boardgamedesigner'])
+    game_dict['categories'] = unpack_list(g['boardgamecategory'])
+    #game_dict['minage']
+    game_dict['name'] = g['name']
+    #game_dict['alternative_names']
+    #game_dict['expands']
+    
+    #Utilizing the stats field
+    stats = g['statistics']['ratings']
+    #ratings statistics
+    game_dict['median'] = float(stats['median'])
+    game_dict['numcomments'] = int(stats['numcomments'])
+    game_dict['stddev'] = float(stats['stddev'])
+    game_dict['wishing'] = int(stats['wishing'])
+    game_dict['usersrated'] = int(stats['usersrated'])
+    game_dict['averageweight'] = float(stats['averageweight'])
+    game_dict['trading'] = int(stats['trading'])
+    game_dict['average'] = float(stats['average'])
+    game_dict['owned'] = int(stats['owned'])
+    game_dict['wanting'] = int(stats['wanting'])
+    game_dict['bayesaverage'] = float(stats['bayesaverage'])
+    game_dict['numweights'] = int(stats['numweights'])
+    
+    #rank transformation
+    rank_dict = []
+    rank_dict[0] = {
+            'friendlyname': 'Board Game Rank',
+            'name': 'boardgame',
+            'value': stats[0]['@value']
+            }
+    rank_dict[1] = {
+            'friendlyname': 'Strategy Game Rank',
+            'name': 'strategygames',
+            'value': stats[1]['@value']
+            }
+    game_dict['ranks'] = rank_dict  
+
+    return game_dict[id], game_dict
+
+def unpack_list(obj_to_eval):
+    if type(obj_to_eval) == list:
+        return [item['#text'] for item in obj_to_eval]
+    else:
+        return obj_to_eval
+
+def etree_to_dict(t):
+
+    d = {t.tag: {} if t.attrib else None}
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, children):
+            for k, v in dc.iteritems():
+                dd[k].append(v)
+        d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
+    if t.attrib:
+        d[t.tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+              d[t.tag]['#text'] = text
+        else:
+            d[t.tag] = text
+    return d
+
+
+
+def format_url(start, stop):
+    url = 'https://boardgamegeek.com/xmlapi/boardgame/'
+    ids_ = ",".join([str(n) for  n in range(start, stop+1)])
+    return url+ids_+'&stats=1'
+
+def pull_game_data_xml(start, stop):
+    url = format_url(start, stop)
+    r = requests.get(url)
+    tree = ElementTree.fromstring(r.content)
+    many_game_dict = etree_to_dict(tree)
+    #parse the xml now
+    for game in many_game_dict['boardgames']['boardgame']:
+        import pdb; pdb.set_trace()
+        try:
+            evaluate_xml(game['boardgame'])
+        except:
+            print game
+            print 'error'
+
+    return(many_game_dict.keys())
+
+
+    
 if __name__=='__main__':
     start = int(sys.argv[1])
     stop = int(sys.argv[2])
@@ -47,4 +160,4 @@ if __name__=='__main__':
     if method == 'library':
         pull_game_data(start, stop)
     else:
-        pass #do the xml thing
+        pull_game_data_xml(start, stop)
