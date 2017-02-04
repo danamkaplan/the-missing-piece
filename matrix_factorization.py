5edf735da89a715d9fc4c3cef653b74a2004f193
Dana Kaplan <danamkaplan@gmail.com>
2017-02-04 15:55:39 -0800

    a bit of cleanup

--- matrix_factorization.py
+++
@@ -1,9 +0,0 @@
-from collect_json import data_pipeline
-import pandas as pd
-import numpy as np
-
-folder = '../data/'
-
-feature_matrix = data_pipeline(folder, 'combine')
-
-U,Sigma,V = np.linalg.svd(feature_matrix.as_matrix())
